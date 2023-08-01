16da2c9d1f736af02af41ff4eaa1be611af633eb
ikuzeyo <134299001+ikuzeyo@users.noreply.github.com>
2023-08-01 22:39:24 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題連想配列</title>
+</head>
+
+<body>
+    <p>
+        <?php
+    $hairetsu =['name'=>'onion','price'=>200,'weight'=>160];
+    print_r($hairetsu);
+
+        ?>
+    </p>
+
+</body>
+
+</html>
